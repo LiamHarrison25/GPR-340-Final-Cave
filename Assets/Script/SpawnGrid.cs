f15d6309828e66ef05cb76595ff4f73e9957619f
Wang Grundy <2101005@uad.ac.uk>
2023-11-14 14:20:05 -0500

    added buckets

--- Assets/Script/SpawnGrid.cs
+++ Assets/Script/SpawnGrid.cs
@@ -8,54 +8,69 @@ public class SpawnGrid : MonoBehaviour
     [SerializeField] private float halfWidth = 0.5f;
     [SerializeField] private GameObject cubePrefab;
     [SerializeField] private Material[] materialList;
-    Vector3Int quantize(Vector3 position, float cellSize) {
+
+    struct Bucket{
+        bool isVisited;
+        Vector3 pos;
+        List<GameObject> objects;
+    }
+
+    //convert vector3f to vector3int
+
+    Vector3Int QuantizeFloatToInt(Vector3 position, float cellSize) {
         return new Vector3Int(
             (int)Mathf.Floor(position.x/cellSize),
             (int)Mathf.Floor(position.y/cellSize),
             (int)Mathf.Floor(position.z/cellSize));
     }
     
-    // change the value to whatever you want to hold
-    private Dictionary<Vector3Int, HashSet<GameObject>> dataGrid;
+    //change the value to whatever you want to hold
+    private Dictionary<Vector3Int, Bucket> dataGrid; 
     
     void Start()
     {
-        int x , y, z;
-        int index = 0;
-        int numAxis = 0;
-        List<Vector3> grid = new List<Vector3>();
-        
-        for (x = 0; x < worldSize; x++)
+        //int numAxis = 0;
+        for (int x = 0; x < worldSize; x++)
         {
-            for (y = 0; y < worldSize; y++)
+            for (int y = 0; y < worldSize; y++)
             {
-                for (z = 0; z < worldSize; z++)
+                for (int z = 0; z < worldSize; z++)
                 {
-                    numAxis = 0;
-                    grid.Add(new Vector3(x, y, z));
-                    
-                    if (z % 10 == 0)
-                    {
-                        numAxis++;
-                    }
-                    
-                    if (x % 10 == 0)
-                    {
-                        numAxis++;
-                    }
-                    
-                    if (y % 10 == 0)
-                    {
-                        numAxis++;
-                    }
-                    
-                    if (numAxis == 3)
-                    {
-                        var obj = Instantiate(cubePrefab);
-                        obj.transform.position = new Vector3(x, y, z);
-                    }
+                    //numAxis = 0;
+                    dataGrid.Add(QuantizeFloatToInt(new Vector3(x, y, z), 1), new Bucket()); 
+
+                    //if (z % 10 == 0)
+                    //{
+                    //    numAxis++;
+                    //}
+
+                    //if (x % 10 == 0)
+                    //{
+                    //    numAxis++;
+                    //}
+
+                    //if (y % 10 == 0)
+                    //{
+                    //    numAxis++;
+                    //}
+
+                    //if (numAxis == 3)
+                    //{
+                    //var obj = Instantiate(cubePrefab);
+                    //obj.transform.position = new Vector3(x, y, z);
+
+                    //}
                 }
             }
         }
     }
+
+    public void OnDrawGismos()
+    {
+
+    }
 }
+
+
+
+
